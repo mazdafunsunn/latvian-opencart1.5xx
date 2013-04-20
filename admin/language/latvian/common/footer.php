224ec27e8ab1ded93c3312f58ddaea3c3a222a5b
Maris Kreitins <tm1717@gmail.com>
2013-04-20 15:36:07 +0300

    sākuma komplekts
    visi standarta faili angļu valodā

---
+++ admin/language/latvian/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer'] = 'Mebstore &copy; 2009-' . date('Y') . '';
+?>
---
+++ admin/language/latvian/feed/salidzini.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']    = 'Salidzini.lv';
+
+// Text   
+$_['text_feed']        = 'Datu padeve uz Salidzini.lv';
+$_['text_success']     = 'Jūs veiksmīgi veicāt izmaiņas!';
+$_['text_select_all'] = 'Izvēlēties visu';
+$_['text_unselect_all'] = 'Noņemt izvēli visam';
+
+// Entry
+$_['entry_status']     = 'Stāvoklis:';
+$_['entry_data_feed']  = 'Datu plūsmas URL:';
+$_['entry_source_language'] = 'Datu plūsmas valoda';
+$_['entry_category']           = 'IZŅĒMUMI:<br/><span class="help">Izvēlieties preču <strong>grupas, kuru preces
+nevēlaties nosūtīt</strong> uz Salidzini.lv. 
+Ja kaut viena no precei piesaistītajām preču grupām būs šajā izņēmuma sarakstā, šī prece netiks
+ievietota datu plūsmā uz Salidzini.lv</span>';
+$_['entry_securitycode']     = 'Drošības kods:<br/><span class="help">Ierakstiet jebkuru ciparu/burtu kombināciju, kas kalpos jums kā papildu drošības kods. URL saite tiks izveidota automātiski un tā ir jāiesniedz salidzini.lv katru reizi, kad jūs to mainat.</span>';
+$_['entry_multipliers']     = 'Cenas korekcija:<br/><span class="help">Katram jūsu veikalam ierakstiet reizinātāju ar kuru vēlaties reizināt katras jūsu preces cenu (pirms nodokļiem). 1 = Cenas korekcija netiks veikta.</span>';
+
+// Error
+$_['error_permission'] = 'Jums nav atļauts veikt šajā sadaļā izmaiņas bez administrācijas atļaujas!';
+?>
